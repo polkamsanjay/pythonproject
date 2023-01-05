b40610b1feb9bd9ac24260044cd23493b4bf29ba
polkamsanjay <polkamsanjay@yahoo.com>
2023-01-04 23:30:30 -0500

    commit

---
+++ Readme.md
@@ -0,0 +1,9 @@
+MAC Address Lookup
+This script performs a lookup of a MAC address using the macaddress.io API. It retrieves the company name, company address, and country code associated with the MAC address and prints the information to the console.
+
+Prerequisites
+You will need an API key from macaddress.io. You can obtain a free API key by signing up for a free account.
+Usage
+Replace API_KEY in the code with your own API key.
+Replace Mac_Address with the MAC address you want to look up.
+Run the script using a command like python macaddress.py.
---
+++ src/macaddress.py
@@ -0,0 +1,22 @@
+import sys
+import requests
+
+mac_address = "44:38:39:ff:ef:57"
+
+# Query MAC address lookup API
+api_key = "at_ymgTVBIb5rgbbsTPFzq2gr7RiMFyJ"
+url = f"https://api.macaddress.io/v1?apiKey={api_key}&output=json&search={mac_address}"
+response = requests.get(url)
+
+# Extract company name from API response
+if response.status_code != 200:
+    print(f"Failed to retrieve information for MAC address {mac_address}")
+    sys.exit(1)
+
+response_json = response.json()
+company_name = response_json["vendorDetails"]["companyName"]
+print(f"Company name: {company_name}")
+company_address = response_json["vendorDetails"]["companyAddress"]
+print(f"Company address: {company_address}")
+country_Code = response_json["vendorDetails"]["countryCode"]
+print(f"country Code: {country_Code}")
